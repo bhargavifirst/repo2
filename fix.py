9ee0a2ace58f8cfeb90052ddc0dcd0407f36db57
bhargavi <siddineni.bhargavi@gmail.com>
2024-06-05 12:39:04 +0530

    trough pycharm

---
+++ fix.py
@@ -0,0 +1,16 @@
+import pytest
+@pytest.fixture()
+def name():
+    s = ("N.bbhargavi", "chandana")
+    return s
+def test_a(name):
+    l=len(name)
+    print(l)
+    print("my name is " + name[1])
+@pytest.fixture()
+def cal():
+    num = (1, 2, 3, 4)
+    return num
+def test_add(cal):
+    a = cal[1]+cal[3]
+    print(a)
